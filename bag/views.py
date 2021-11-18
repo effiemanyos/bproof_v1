e8b1dfc39fb691de9f32ccda55fdf49f59c4889e
Effie Manyos <fmanyos@gmail.com>
2021-11-18 10:30:59 +0000

    Add adjust bad and remove drom bag views, and calc subtotal template filter

--- bag/views.py
+++ bag/views.py
@@ -49,12 +49,11 @@ def remove_from_bag(request, item_id):
         bag = request.session.get('bag', {})
         
         bag.pop(item_id)
-        messages.success(request, f'Removed {product.name} from your bag')
+        messages.success(request,(f'Removed {product.name} from your bag'))
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
 
-    except Exception as e:
-        messages.error(request, f'Error removing item: {e}')
-        request.session['bag'] = bag
+    except Exception as error:
+        messages.error(request, f'Error removing item: {error}')
         return HttpResponse(status=500)
